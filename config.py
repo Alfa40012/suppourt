5429fb19c1a4289ef1abe31c937988fc8265f2fc
Alfa40012 <alfa40012@gmail.com>
2025-11-25 16:40:50 +0200

    Update welcome message and add new links

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 
+# رسالة الترحيب في الخاص
 WELCOME_MSG = """
-🎉 أهلاً بك في دعم CEMEX 3030  
+🎉 أهلاً بك في دعم SUPPORT TV  
 اختر من القائمة التالية:
 
 1️⃣ اشتراك جديد  
@@ -14,3 +14,7 @@ WELCOME_MSG = """
 4️⃣ تحميل ملف القنوات  
 5️⃣ التواصل مع خدمة العملاء  
 """
+
+WHATSAPP_LINK = "https://wa.me/message/2JZ4HHC5JOSFC1"
+
+LATEST_CHANNELS_FILE = "https://www.mediafire.com/file/2x3zvhbrg0pz8lh"  # ملف القنوات
